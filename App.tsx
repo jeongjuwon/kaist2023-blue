88df576c2cbe739c50cd3561030424952a71e90e
Juwon Jeong <2jw0718@gmail.com>
2023-10-06 20:54:25 +0900

    update

--- App.tsx
+++ App.tsx
@@ -6,10 +6,18 @@
  */
 
 import CommonText from '@/components/CommonText';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView} from 'react-native';
+import RNBootSplash from 'react-native-bootsplash';
 
 function App(): JSX.Element {
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('3초 뒤에 실행됩니다.');
+      RNBootSplash.hide();
+    }, 3000);
+  }, []);
+
   return (
     <SafeAreaView>
       <CommonText>안녕하세요.</CommonText>
--- ios/KaistBlue/AppDelegate.mm
+++ ios/KaistBlue/AppDelegate.mm
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import "RNBootSplash.h"
 
 #import <React/RCTBundleURLProvider.h>
 
@@ -22,5 +23,18 @@
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
+
+- (UIView *)createRootViewWithBridge:(RCTBridge *)bridge
+                          moduleName:(NSString *)moduleName
+                           initProps:(NSDictionary *)initProps {
+  UIView *rootView = [super createRootViewWithBridge:bridge
+                                          moduleName:moduleName
+                                           initProps:initProps];
+
+  [RNBootSplash initWithStoryboard:@"LaunchScreen" rootView:rootView]; // ⬅️ initialize the splash screen
+
+  return rootView;
+}
+
 
 @end
